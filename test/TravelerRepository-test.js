43f29d042cbdc99e300e72c51ce51d4309d9cfd2
Rachel Allen <rallenco13@gmail.com>
2022-06-11 13:20:33 -0600

    set up TravelerRepository-test.js file and beforeEach

---
+++ test/TravelerRepository-test.js
@@ -0,0 +1,41 @@
+import { expect } from "chai";
+import Traveler from "../src/Traveler";
+import TravelerRepository from "../src/TravelerRepository";
+
+describe("Traveler Repository", () => {
+  let traveler1;
+  let traveler2;
+  let travelerRepository;
+  beforeEach(() => {
+    traveler1 = new Traveler({
+      id: 1,
+      name: "Ham Leadbeater",
+      travelerType: "relaxer"
+    });
+
+    traveler2 = new Traveler({
+      id: 2,
+      name: "Rachael Vaughten",
+      travelerType: "thrill-seeker"
+    });
+    travelerRepository = new TravelerRepository([traveler1, traveler2]);
+  });
+
+  it.only("should be a function", function() {
+    expect(TravelerRepository).to.be.a("function");
+  });
+
+  it("should be an instance of TravelerRepository", () => {
+    expect(travelerRepository).to.be.an.instanceof(TravelerRepository);
+  });
+
+  it("should be able to determine traveler data by ID", () => {
+    let determineTraveler = travelerRepository.findTraveler(traveler2.id);
+    expect(determineTraveler).to.equal(traveler2);
+  });
+
+  it("should be able to capture the average step goal of the travelers ", () => {
+    let averageSteps = travelerRepository.calculateAvgStepGoal();
+    expect(averageSteps).to.equal(6500);
+  });
+});
