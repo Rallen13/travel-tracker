db9499b697cb1d0f5d357b63fc5e301987784671
Rachel Allen <rallenco13@gmail.com>
2022-06-11 16:14:30 -0600

    added getRandomIndex function to load a random travelers data until login is wired up

--- src/scripts.js
+++ src/scripts.js
@@ -1,9 +1,23 @@
+// Imports
 import "normalize.css";
 import "./css/styles.css";
 import apiCalls from "./apiCalls";
 import "./images/hiking_black_24dp.svg";
 
 console.log("This is the JavaScript entry file - your code begins here.");
+
+// Query Selectors
+
+// Form Query Selectors
+
+// Class Instances
+
+//Global Variables
+
+// Functions
+const getRandomIndex = array => {
+  return Math.floor(Math.random() * array.length + 1);
+};
 
 const fetchApiCalls = userID => {
   apiCalls.fetchData().then(data => {
@@ -13,3 +27,5 @@ const fetchApiCalls = userID => {
 
 // Event Listeners
 window.addEventListener("load", fetchApiCalls);
+
+//Form Event Listeners
